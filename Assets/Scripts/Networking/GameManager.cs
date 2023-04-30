bb2aec72c041a375c68bbc3aee7c5d4d81277450
Brad Dallimer <braddallimer@gmail.com>
2023-04-30 12:15:31 +0100

    Finished working on lobby ui and game manager. Continuing to work on player and player networking.

---
+++ Assets/Scripts/Networking/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Brad.KeepyUp.Networking
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        #region Photon Callbacks
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", newPlayer.NickName); // not seen if you're the player connecting.
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", otherPlayer.NickName); // seen when other disconnects
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // Called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
+        }
+
+        // Called when the local player left the room. We need to load the launcher scene.
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+        #endregion
+
+        #region Private Methods
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.Log("PhotonNetwork: Trying to load a level but we are not the master client.");
+                return;
+            }
+            Debug.LogFormat("PhotonNetwork: Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Multiplayer Room");
+        }
+        #endregion
+
+        #region Public Methods
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+        #endregion
+    }
+}
+
--- Assets/Scripts/Networking/Launcher.cs
+++ Assets/Scripts/Networking/Launcher.cs
@@ -29,6 +29,11 @@ namespace Brad.KeepyUp.Networking
         // This client's version number. Users are seperated from each other by gameVersion (which allows you
         // to make breaking changes).
         string gameVersion = "1";
+
+        // Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photo,
+        // we need to keep track of this to properly adjust the behaviour when we receive a callback by Photon.
+        // Typically this is used for the OnConnectedToMaster() callback.
+        bool isConnecting;
         #endregion
 
         #region MonoBehaviour Callbacks
@@ -51,15 +56,24 @@ namespace Brad.KeepyUp.Networking
         #region MonoBehaviourPunCallbacks Callbacks
         public override void OnConnectedToMaster()
         {
-            Debug.Log("NETWORKING/Launcher: OnConnectedToMaster() was called by PUN");
+            // We don't want to do anything if we are not attempting to join a room.
+            // This case where isConnecting is false is typically when you lost or quit the game, when this level is loaded,
+            // OnConnectedToMaster will be called, in that case we don't do anything.
+            if (isConnecting)
+            {
+                Debug.Log("NETWORKING/Launcher: OnConnectedToMaster() was called by PUN");
 
-            // #CRITICAL - The first we try to do is to join a potential existing room. If there is, good, else,
-            // we'll be called back with OnJoinRandomFailed().
-            PhotonNetwork.JoinRandomRoom();
+                // #CRITICAL - The first we try to do is to join a potential existing room. If there is, good, else,
+                // we'll be called back with OnJoinRandomFailed().
+                PhotonNetwork.JoinRandomRoom();
+                isConnecting = false;
+            }
         }
 
         public override void OnDisconnected(DisconnectCause cause)
         {
+            isConnecting = false;
+            
             progressLabel.SetActive(false);
             controlPanel.SetActive(true);
 
@@ -78,6 +92,16 @@ namespace Brad.KeepyUp.Networking
         public override void OnJoinedRoom()
         {
             Debug.Log("NETWORKING/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+            // #CRITICAL - We only load if we are the first player, else we rely on 'PhotonNetwork.AutomaticallySyncScene' to
+            // sync our instance scene.
+            if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                Debug.Log("We load room");
+
+                // #CRITICAL - Load the Multiplayer_Room level
+                PhotonNetwork.LoadLevel("Multiplayer_Room");
+            }
         }
         #endregion
 
@@ -101,6 +125,10 @@ namespace Brad.KeepyUp.Networking
             }
             else
             {
+                // Keep track of the will to join a room, because wehn we come back from the game we
+                // will get a callback that we are connected, so we need to know what to do then.
+                isConnecting = PhotonNetwork.ConnectUsingSettings();
+
                 // #CRITICAL - We must first and foremost connect to Photon Online Server.
                 PhotonNetwork.ConnectUsingSettings();
                 PhotonNetwork.GameVersion = gameVersion;
